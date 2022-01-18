ddb8c45fe73eb65769600011a67cbe8391e15e74
Tobias Löbermann <tobias.loebermann@cosmosdirekt.de>
2022-01-18 14:33:36 +0100

    remove unused code

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,20 @@
 const snapshotSerializers = require('jest-preset-angular/build/serializers');
 
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
   preset: 'jest-preset-angular',
   snapshotSerializers,
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png)$': '<rootDir>/__mocks__/image.js',
-    '^@lib/(.*)$': '<rootDir>/src/lib/$1',
-  },
   setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
   transform: {
-    '^.+\\.js': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!@ionic-native|@ionic|@ionic/core|ionic-angular|@angular)',
-    '.+\\.json',
-  ],
+  transformIgnorePatterns:
+    ['node_modules/(?!.*\\.mjs$)']
+   ,
   testPathIgnorePatterns: ['/node_modules/'],
-  modulePathIgnorePatterns: ['<rootDir>/__mocks__', '<rootDir>/src/.*/__mocks__'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,ts}'],
-  coverageReporters: [['lcov', { projectRoot: '../' }], 'text-summary'],
 };
--- src/setupJest.ts
+++ src/setupJest.ts
@@ -1,14 +1,8 @@
-import 'zone.js';
-import 'zone.js/testing';
-// import 'zone.js/dist/zone-testing';
-import 'jest-preset-angular';
+import 'zone.js/fesm2015/zone-testing-bundle.min';
 import './jestGlobalMocks';
-import 'core-js/es/reflect';
-import 'core-js/proposals/reflect-metadata';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import {getTestBed} from '@angular/core/testing';
 
-// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
